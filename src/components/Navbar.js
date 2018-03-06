0d70f64fa3003df79dc855527df129d037a0fd65
Yaphet Abraha <yaphet.abraha@gmail.com>
2018-03-06 07:15:45 -0600

    Responsive fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <a
-        href="#home"
+        href="#"
         onClick={() => animateScrollTo(document.querySelector(".home"))}
       >
         Home
       </a>
-      <a href="#portfolio">portfolio</a>
+      <a href="#portfolio">Portfolio</a>
       <a
         href="#contact"
         onClick={() => animateScrollTo(document.querySelector(".contact"))}

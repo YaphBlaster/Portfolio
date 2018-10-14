4a6b8d50ec058c7ea002890fa18ad65f40b96ca4
Yaphet Abraha <>
2018-10-13 19:06:46 -0500

    removed old code

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,24 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const createMarkup = input => {
-  if (Array.isArray(input)) {
-    let returnString;
-    let count = 0;
-    input.forEach(element => {
-      if (count !== 0) {
-        returnString += ` ${element}`;
-      } else {
-        returnString = `${element}`;
-      }
-      count++;
-    });
-    return { __html: `${returnString}` };
-  } else {
-    return { __html: input };
-  }
-};
-
 const ProjectItem = props => {
   return (
     <div className="project-item">

02244ebe82cd0ac3a947f99848c8f1b212d0b036
Yaphet Abraha <yaphet.abraha@gmail.com>
2019-01-30 21:24:11 -0600

    Hypemoji added to portfolio

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -12,16 +12,28 @@ const ProjectContainer = props => {
 
       <div className="project-container">
         {props.projects.map((project, index) => {
+          const {
+            title,
+            description,
+            techStack,
+            github,
+            gitText,
+            demo,
+            demoText,
+            year,
+            image
+          } = project;
           return (
             <ProjectItem
-              title={project.title}
-              description={project.description}
-              techStack={project.techStack}
-              github={project.github}
-              demo={project.demo}
-              demoText={project.demoText}
-              year={project.year}
-              image={project.image}
+              title={title}
+              description={description}
+              techStack={techStack}
+              github={github}
+              gitText={gitText}
+              demo={demo}
+              demoText={demoText}
+              year={year}
+              image={image}
               key={index}
             />
           );
